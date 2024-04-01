00ccf81575de6e0ac46f79bca874ee21d5422fc0
Gabriel <gabo5612@gmail.com>
2024-04-01 09:30:41 -0400

    structure

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
-<?php wp_footer(); 
+<?php wp_footer(); ?>
+
+<?php
 $brandsLogos= getfield('brands_logos');
 $footerResources = wp_get_nav_menu_items('Footer_Resources');
 $footerCompany = wp_get_nav_menu_items('Footer_Company');
-?>
+ ?>
 <script>
 jQuery(window).scroll(function() {
     if (jQuery(this).scrollTop() > 60){  
