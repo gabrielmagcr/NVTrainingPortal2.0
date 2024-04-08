c49d21c5c69fd6478ad77b8c48c25e4d63eceae0
Gabriel <gabo5612@gmail.com>
2024-04-08 08:20:24 -0400

    page home test

--- page-home.php
+++ page-home.php
@@ -221,11 +221,11 @@ get_header();
 </script>
 <?php } ?>
 </div>
+</div>
+</div>
 <?php if (!is_user_logged_in()) { ?>
 	<div class="bg-botton-logout"></div>
 <?php } ?>
-</div>
-</div>
 </div>
 <?php
 get_footer();
