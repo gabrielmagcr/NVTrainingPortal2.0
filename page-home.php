35899ab99095934dfaa954d45388e46a0ffd4cab
Gabriel <gabo5612@gmail.com>
2024-04-09 17:30:35 -0400

    fix

--- page-home.php
+++ page-home.php
@@ -173,7 +173,7 @@ get_header();
 
 					<div class="productCategoriesContainer">
 						<div class="mainVideoText">
-							<h3 class="welcomeTitle">Product lines</h2>
+							<h2 class="welcomeTitle productText">Product Categories</h2>
 								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 						</div>
 					</div>
