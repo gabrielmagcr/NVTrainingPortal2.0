24c78f51875e26086211d0be420b342ed9ec9bc3
Gabriel <gabo5612@gmail.com>
2024-04-10 17:22:30 -0400

    footer btn

--- footer.php
+++ footer.php
@@ -167,6 +167,15 @@
         color: #fff;
     }
 
+    .newsletterbtn {
+        color: #fff;
+        background-color: #E26743;
+        border: none;
+        border-radius: 35px;
+        padding: 10px 30px;
+
+    }
+
     @media (min-width:400px) {
         .footerLogos img {
             height: 45px;
@@ -258,9 +267,7 @@
         }
 
         .newsLetterFormClass input,
-        .newsletterbtn {
-            margin: 40px 0;
-        }
+
 
         .responsiveList a {
             color: #ffffff;
@@ -366,7 +373,7 @@
             <span>NEWSLETTER SIGN UP</span>
             <div class="newsLetterFormulary">
                 <input id="newsLetterForm" type="email" autocapitalize="off" autocomplete="email" placeholder="Email Address">
-                <button class="top-btns newsletterbtn">
+                <button class="newsletterbtn">
                     <svg viewBox="0 0 14 10" fill="none" aria-hidden="true" focusable="false" role="presentation" class="icon icon-arrow" width='35px' xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M8.537.808a.5.5 0 01.817-.162l4 4a.5.5 0 010 .708l-4 4a.5.5 0 11-.708-.708L11.793 5.5H1a.5.5 0 010-1h10.793L8.646 1.354a.5.5 0 01-.109-.546z" fill="currentColor">
                         </path>
