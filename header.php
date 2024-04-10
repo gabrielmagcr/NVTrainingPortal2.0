69e54d696cd0c4af8927ac7a7cd660b1fdf3f0e9
Gabriel <gabo5612@gmail.com>
2024-04-10 11:50:47 -0400

    test

--- header.php
+++ header.php
@@ -476,18 +476,17 @@
         </div><!--end responsive-menu-->
 
         <script>
-       jQuery(".close-btn").on("click", function() {
-    jQuery(".responsive-menu").animate({
-        left: '-100%'
-    }, 'slow');
-});
+          jQuery(".close-btn").on("click", function() {
+            jQuery(".responsive-menu").animate({
+              left: '-100%'
+            }, 'slow');
+          });
 
-jQuery(".bars").on("click", function() {
-    jQuery(".responsive-menu").css('left', '-100%').animate({
-        left: '0%'
-    }, 'slow');
-});
-
+          jQuery(".bars").on("click", function() {
+            jQuery(".responsive-menu").css('left', '-100%').animate({
+              left: '0%'
+            }, 'slow');
+          });
         </script>
       </div><!--end header-ll-->
       <div class="header-c">
