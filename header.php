5caa153307cba1b34b779f3ec591235655160a10
Gabriel <gabo5612@gmail.com>
2024-04-10 11:52:42 -0400

    test

--- header.php
+++ header.php
@@ -476,20 +476,22 @@
         </div><!--end responsive-menu-->
 
         <script>
-         jQuery(".close-btn").on("click", function() {
-    jQuery(".responsive-menu").animate({
-        left: '-100%'
-    }, 'slow', function() {
-        jQuery(this).hide();
-    });
-});
+          jQuery(".close-btn").on("click", function() {
+            jQuery(".responsive-menu").animate({
+              left: '-100%'
+            }, 'slow', function() {
+              jQuery(this).hide();
+            });
+          });
 
-jQuery(".bars").on("click", function() {
-    jQuery(".responsive-menu").css('left', '-100%').show().animate({
-        left: '0%'
-    }, 'slow');
-});
-
+          jQuery(".bars").on("click", function() {
+            jQuery(".responsive-menu").css({
+              display: 'block',
+              left: '-100%'
+            }).animate({
+              left: '0%'
+            }, 'slow');
+          });
         </script>
       </div><!--end header-ll-->
       <div class="header-c">
