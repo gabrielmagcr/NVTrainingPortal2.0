e0f4279298837491a14977126a6ac41912bb02f0
Gabriel <gabo5612@gmail.com>
2024-04-10 11:48:17 -0400

    smooth

--- header.php
+++ header.php
@@ -476,19 +476,13 @@
         </div><!--end responsive-menu-->
 
         <script>
-          jQuery(".close-btn").on("click", function() {
-            jQuery(".responsive-menu").animate({
-              left: '-100%'
-            }, 'slow', function() {
-              jQuery(this).hide();
-            });
-          });
+         jQuery(".close-btn").on("click", function() {
+    jQuery(".responsive-menu").slideUp('slow');
+});
 
-          jQuery(".bars").on("click", function() {
-            jQuery(".responsive-menu").css('left', '-100%').show().animate({
-              left: '0%'
-            }, 'slow');
-          });
+jQuery(".bars").on("click", function() {
+    jQuery(".responsive-menu").slideDown('slow');
+});
         </script>
       </div><!--end header-ll-->
       <div class="header-c">
