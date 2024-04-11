0fc0fff9ec1fc5045972e950bc84d4aac2435098
Gabriel <gabo5612@gmail.com>
2024-04-11 11:34:30 -0400

    fix

--- productLine.php
+++ productLine.php
@@ -8,7 +8,7 @@
             justify-content: space-evenly;
         }
 
-        .placeholder-img, .productLineImg video {
+        .placeholder-imgPL, .productLineImg video {
             width: 315px;
 			height: 185px;
         }
@@ -34,7 +34,7 @@
     <div class="productVideoContainer">
         <div class="mainVideo productLineVideo">
             <div class="mainVideoImg productLineImg">
-                <img class="placeholder-img" src="/wp-content/uploads/Placeholder-image-2.png" alt="Placeholder Image">
+                <img class="placeholder-imgPL" src="/wp-content/uploads/Placeholder-image-2.png" alt="Placeholder Image">
                 <img class="play-icon playIconProductVideo"  src="/wp-content/uploads/Button-play.svg" alt="Play Button">
                 <video class="video" controls style="display: none;">
                     <source src="/wp-content/uploads/2018/11/HempVideo.mp4" type="video/mp4">
@@ -47,7 +47,7 @@
         </div>
         <div class="mainVideo productLineVideo">
             <div class="mainVideoImg productLineImg">
-                <img class="placeholder-img" src="/wp-content/uploads/Placeholder-image-3.png">
+                <img class="placeholder-imgPL" src="/wp-content/uploads/Placeholder-image-3.png">
                 <img class="play-icon playIconProductVideo"  src="/wp-content/uploads/Button-play.svg" alt="Play Button">
                 <video class="video" controls style="display: none;">
                     <source src="/wp-content/uploads/2018/11/HempVideo.mp4" type="video/mp4">
@@ -60,7 +60,7 @@
         </div>
         <div class="mainVideo productLineVideo">
             <div class="mainVideoImg productLineImg">
-                <img class="placeholder-img" src="/wp-content/uploads/Placeholder-image-4.png">
+                <img class="placeholder-imgPL" src="/wp-content/uploads/Placeholder-image-4.png">
                 <img class="play-icon playIconProductVideo"  src="/wp-content/uploads/Button-play.svg" alt="Play Button">
                 <video class="video" controls style="display: none;">
                     <source src="/wp-content/uploads/2018/11/HempVideo.mp4" type="video/mp4">
