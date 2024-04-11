a0eb0e1afc933ea7674b3e2452a8dcc6a3203955
Gabriel <gabo5612@gmail.com>
2024-04-11 10:32:49 -0400

    test

--- mainVideos.php
+++ mainVideos.php
@@ -12,13 +12,15 @@
 
 	.mainVideoImg video {
 		width: 330px;
-		height: 185px;
+	
 		border-radius: 24px;
 	}
-	.placeholder-img{
+
+	.placeholder-img {
 		width: 330px;
-		height: 185px;
+	
 	}
+
 	.mainVideoText {
 		text-align: left;
 		width: 80%;
@@ -63,7 +65,8 @@
 			height: 285px;
 			align-items: flex-start;
 		}
-		.placeholder-img{
+
+		.placeholder-img {
 			width: 500px;
 			height: 285px;
 		}
@@ -71,63 +74,64 @@
 		.mainVideoText {
 			width: auto;
 		}
+
 		.playIconMainVideo {
-    width: 76px;
-}
+			width: 76px;
+		}
 	}
 </style>
 
 <div class="mainVideosContainer">
-    <div class="mainVideo">
-        <div class="mainVideoImg">
-            <img class="placeholder-img" src="/wp-content/uploads/Placeholder-image.png" alt="Placeholder Image">
-            <img class="play-icon playIconMainVideo" src="/wp-content/uploads/Button-play.svg" alt="Play Button">
-            <video class="video" controls style="display: none;">
-                <source src="/wp-content/uploads/2018/11/HempVideo.mp4" type="video/mp4">
-            </video>
-        </div>
-        <div class="mainVideoText">
-            <h3>Our Brand - Naturevet Brand Video</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-    </div>
-    <div class="mainVideo">
-        <div class="mainVideoImg">
-            <img class="placeholder-img" src="/wp-content/uploads/Placeholder-image-1.png" alt="Placeholder Image">
-            <img class="play-icon playIconMainVideo" src="/wp-content/uploads/Button-play.svg" alt="Play Button">
-            <video class="video" controls style="display: none;">
-                <source src="/wp-content/uploads/2018/11/HempVideo.mp4" type="video/mp4">
-            </video>
-        </div>
-        <div class="mainVideoText">
-            <h3>Manufacturing Quality Video</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-    </div>
+	<div class="mainVideo">
+		<div class="mainVideoImg">
+			<img class="placeholder-img" src="/wp-content/uploads/Placeholder-image.png" alt="Placeholder Image">
+			<img class="play-icon playIconMainVideo" src="/wp-content/uploads/Button-play.svg" alt="Play Button">
+			<video class="video" controls style="display: none;">
+				<source src="/wp-content/uploads/2018/11/HempVideo.mp4" type="video/mp4">
+			</video>
+		</div>
+		<div class="mainVideoText">
+			<h3>Our Brand - Naturevet Brand Video</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		</div>
+	</div>
+	<div class="mainVideo">
+		<div class="mainVideoImg">
+			<img class="placeholder-img" src="/wp-content/uploads/Placeholder-image-1.png" alt="Placeholder Image">
+			<img class="play-icon playIconMainVideo" src="/wp-content/uploads/Button-play.svg" alt="Play Button">
+			<video class="video" controls style="display: none;">
+				<source src="/wp-content/uploads/2018/11/HempVideo.mp4" type="video/mp4">
+			</video>
+		</div>
+		<div class="mainVideoText">
+			<h3>Manufacturing Quality Video</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		</div>
+	</div>
 </div>
 
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const placeholders = document.querySelectorAll('.placeholder-img');
-        const playIcons = document.querySelectorAll('.play-icon');
-        const videos = document.querySelectorAll('.video');
+	document.addEventListener('DOMContentLoaded', function() {
+		const placeholders = document.querySelectorAll('.placeholder-img');
+		const playIcons = document.querySelectorAll('.play-icon');
+		const videos = document.querySelectorAll('.video');
 
-        for (let i = 0; i < playIcons.length; i++) {
-            placeholders[i].addEventListener('click', function() {
-                placeholders[i].style.display = 'none';
-                playIcons[i].style.display = 'none';
-                videos[i].style.display = 'block';
-                videos[i].play();
-            });
+		for (let i = 0; i < playIcons.length; i++) {
+			placeholders[i].addEventListener('click', function() {
+				placeholders[i].style.display = 'none';
+				playIcons[i].style.display = 'none';
+				videos[i].style.display = 'block';
+				videos[i].play();
+			});
 
-            playIcons[i].addEventListener('click', function() {
-                placeholders[i].style.display = 'none';
-                playIcons[i].style.display = 'none';
-                videos[i].style.display = 'block';
-                videos[i].play();
-            });
-        }
-    });
+			playIcons[i].addEventListener('click', function() {
+				placeholders[i].style.display = 'none';
+				playIcons[i].style.display = 'none';
+				videos[i].style.display = 'block';
+				videos[i].play();
+			});
+		}
+	});
 </script>
 
 <?php get_template_part('lineBreak'); ?>
