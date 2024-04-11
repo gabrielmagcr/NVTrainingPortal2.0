94a3782cfee57fcd4c34c2c52329e75a6d9f6c88
Gabriel <gabo5612@gmail.com>
2024-04-11 10:12:01 -0400

    test

--- productLine.php
+++ productLine.php
@@ -72,30 +72,31 @@
         </div>
     </div>
 </div>
-
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         const placeholders = document.querySelectorAll('.placeholder-img');
         const playIcons = document.querySelectorAll('.play-icon');
-        const videos = document.querySelectorAll('.video');
 
         for (let i = 0; i < playIcons.length; i++) {
             placeholders[i].addEventListener('click', function() {
                 placeholders[i].style.display = 'none';
                 playIcons[i].style.display = 'none';
-                videos[i].style.display = 'block';
-                videos[i].play();
+                const video = document.getElementById('video-' + (i + 1));
+                video.style.display = 'block';
+                video.play();
             });
 
             playIcons[i].addEventListener('click', function() {
                 placeholders[i].style.display = 'none';
                 playIcons[i].style.display = 'none';
-                videos[i].style.display = 'block';
-                videos[i].play();
+                const video = document.getElementById('video-' + (i + 1));
+                video.style.display = 'block';
+                video.play();
             });
         }
     });
 </script>
+
 
 
 <?php get_template_part('lineBreak'); ?>
