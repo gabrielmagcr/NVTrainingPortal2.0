ddf7f46c0e50e7c55fccdc2f26e2fcdd52e1acbd
Gabriel <gabo5612@gmail.com>
2024-04-11 13:45:01 -0400

    dinamic info

--- productLine.php
+++ productLine.php
@@ -30,46 +30,62 @@
         }
     }
 </style>
+
+<?php 
+$productionSectionTitle= get_field('productionSectionTitle');
+$productVideo1 = get_field('productVideo1');
+$pPlaceHolderImg1 = get_field('pPlaceHolderImg1');
+$pVideoTitle1 = get_field('pVideoTitle1');
+$pVideoText1 = get_field('pVideoText1');
+$productVideo2 = get_field('productVideo2');
+$pPlaceHolderImg2 = get_field('pPlaceHolderImg2');
+$pVideoTitle2 = get_field('pVideoTitle2');
+$pVideoText2 = get_field('pVideoText2');
+$productVideo3 = get_field('productVideo3');
+$pPlaceHolderImg3 = get_field('pPlaceHolderImg3');
+$pVideoTitle3 = get_field('pVideoTitle3');
+$pVideoText3 = get_field('pVideoText3');
+?>
 <div class="productLineContainer">
-    <h2 class="welcomeTitle">Product lines</h2>
+    <h2 class="welcomeTitle"><?php echo $productionSectionTitle ?></h2>
     <div class="productVideoContainer">
         <div class="mainVideo productLineVideo">
             <div class="mainVideoImg productLineImg">
-                <img class="placeholder-img placeholder-imgPL " src="/wp-content/uploads/Placeholder-image-2.png" alt="Placeholder Image">
+                <img class="placeholder-img placeholder-imgPL " src="<?php echo $pPlaceHolderImg1 ?>" alt="Placeholder Image">
                 <img class="play-icon playIconProductVideo"  src="/wp-content/uploads/Button-play.svg" alt="Play Button">
                 <video class="video" controls style="display: none;">
-                    <source src="/wp-content/uploads/2018/11/HempVideo.mp4" type="video/mp4">
+                    <source src="<?php echo $productVideo1 ?>" type="video/mp4">
                 </video>
             </div>
             <div class="mainVideoText productLineText">
-                <h3>Scoopables - Intro</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <h3><?php echo $pVideoTitle1 ?></h3>
+                <p>L<?php echo  $pVideoText1 ?></p>
             </div>
         </div>
-        <div class="mainVideo productLineVideo">
+		<div class="mainVideo productLineVideo">
             <div class="mainVideoImg productLineImg">
-                <img class="placeholder-img placeholder-imgPL" src="/wp-content/uploads/Placeholder-image-3.png">
+                <img class="placeholder-img placeholder-imgPL " src="<?php echo $pPlaceHolderImg2 ?>" alt="Placeholder Image">
                 <img class="play-icon playIconProductVideo"  src="/wp-content/uploads/Button-play.svg" alt="Play Button">
                 <video class="video" controls style="display: none;">
-                    <source src="/wp-content/uploads/2018/11/HempVideo.mp4" type="video/mp4">
+                    <source src="<?php echo $productVideo2 ?>" type="video/mp4">
                 </video>
             </div>
             <div class="mainVideoText productLineText">
-                <h3>Evolutions - Intro</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <h3><?php echo $pVideoTitle2 ?></h3>
+                <p>L<?php echo  $pVideoText2 ?></p>
             </div>
         </div>
-        <div class="mainVideo productLineVideo">
+		<div class="mainVideo productLineVideo">
             <div class="mainVideoImg productLineImg">
-                <img class="placeholder-img placeholder-imgPL" src="/wp-content/uploads/Placeholder-image-4.png">
+                <img class="placeholder-img placeholder-imgPL " src="<?php echo $pPlaceHolderImg3 ?>" alt="Placeholder Image">
                 <img class="play-icon playIconProductVideo"  src="/wp-content/uploads/Button-play.svg" alt="Play Button">
                 <video class="video" controls style="display: none;">
-                    <source src="/wp-content/uploads/2018/11/HempVideo.mp4" type="video/mp4">
+                    <source src="<?php echo $productVideo3 ?>" type="video/mp4">
                 </video>
             </div>
             <div class="mainVideoText productLineText">
-                <h3>Breed Specific - Intro</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <h3><?php echo $pVideoTitle3 ?></h3>
+                <p>L<?php echo  $pVideoText3 ?></p>
             </div>
         </div>
     </div>
