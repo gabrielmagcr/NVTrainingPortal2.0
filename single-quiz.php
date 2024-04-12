068bc4d4853e6b153180701f014852202cdc0eb4
Gabriel <gabo5612@gmail.com>
2024-04-12 13:51:50 -0400

    test

--- single-quiz.php
+++ single-quiz.php
@@ -191,11 +191,22 @@ while (have_posts()) {
         <!--Line 45 adds the "Quiz text" field associated to quiz in the "Quizzes" Tab in the back end-->
         <?php the_field('quiz_text'); ?>
 
-       <button class="btn" id="toggleVideo">Want to watch the video again?</button>
-<div class="collapse" id="collapseVideo">
-  <!-- PHP para mostrar el video -->
-  <?php the_field('quiz_video'); ?>
-</div>
+        <?php
+$show_video_container = false;
+
+$alert_class = 'alert-error'; 
+
+if ($alert_class === 'alert-error') {
+    $show_video_container = true; /
+}
+?>
+
+<?php if ($show_video_container): ?>
+    <button class="btn" id="toggleVideo">Want to watch the video again?</button>
+    <div class="collapse" id="collapseVideo">
+        <?php the_field('quiz_video'); ?>
+    </div>
+<?php endif; ?>
 
 
         <?php
@@ -228,6 +239,8 @@ get_footer();
 
 
 <script>
+
+  
 document.addEventListener('DOMContentLoaded', function() {
 
   var toggleButton = document.getElementById('toggleVideo');
