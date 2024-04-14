99a00eccef7e8b3fa142ed58f663bdb79b7d22d1
Gabriel <gabo5612@gmail.com>
2024-04-14 18:41:46 -0400

    test2

--- js/videoModal.js
+++ js/videoModal.js
@@ -55,7 +55,7 @@ function checkTime(){
   function removeVideoModal(){
       var modal=document.getElementsByClassName("popup");
       modal[0].style.display='none'
-      console.log('test video')
+      console.log('test videddo')
    
       bgQuizz.style.display='none';
       collapseVideo.style.display='none';
