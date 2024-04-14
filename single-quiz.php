f54fd2feff4b723ce61bd7b0b4c60287697e1a96
Gabriel <gabo5612@gmail.com>
2024-04-14 17:53:16 -0400

    fix quiz shadow

--- single-quiz.php
+++ single-quiz.php
@@ -96,6 +96,10 @@
     border: 1px solid;
     padding: 15px;
     border-radius: 12px;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  }
+  .caldera-grid:last-child, .form-group:last-child{
+    box-shadow: none;
   }
 
   .submitDiv {
