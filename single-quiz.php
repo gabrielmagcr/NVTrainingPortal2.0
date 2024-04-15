0c99967f60a16e0125e2bc8aef9efa29741053a3
Gabriel <gabo5612@gmail.com>
2024-04-15 19:19:47 -0400

    no botton

--- single-quiz.php
+++ single-quiz.php
@@ -231,17 +231,6 @@ while (have_posts()) {
     </div>
   </div>
 
-  <div class="container container--narrow page-section">
-    <div id="bottomHomeButton" class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo get_bloginfo('url'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to all trainings</a> <span class="metabox__main"><?php the_title(); ?></span></p>
-    </div>
-
-  </div>
-
-  <div class="helpLinkButton">
-    <a href=mailto:training@naturvet.com><button class="btn btn-success helpButton">Have a Problem? Contact Us</button>
-    </a>
-  </div>
 
 <?php }
 
