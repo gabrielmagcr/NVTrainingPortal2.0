65dbf9d6eaac74fbed2e8cad33192a96da28f194
Gabriel <gabo5612@gmail.com>
2024-04-17 12:20:16 -0400

    test

--- single-quiz.php
+++ single-quiz.php
@@ -164,96 +164,89 @@
 </style>
 
 <?php
+include 'page-home.php';
 
-include '/page-home.php';
-
+// Verifica si las variables de cuestionarios completados y cuestionarios totales están definidas
 if (isset($completed_quizzes_count) && isset($total_quizzes_count)) {
- $completedText= "Great Job, you’ve completed <?php echo $completed_quizzes_count; ?>/<?php echo $total_quizzes_count; ? quizes!!";
+    $completedText = "Great Job, you’ve completed $completed_quizzes_count/$total_quizzes_count quizzes!!";
 } else {
- $completedText= 'Great Job, you’ve completed this quiz!!';
-}?>
+    $completedText = 'Great Job, you’ve completed this quiz!!';
+}
+?>
+
 <?php
 if (!get_current_user_id()) {
-  wp_redirect(home_url());
+    wp_redirect(home_url());
 }
-//Line 2-4 checks if thier is a active user, else redirect unknown user to home page
+// Verifica si hay un usuario activo, de lo contrario, redirige al usuario desconocido a la página de inicio
 get_template_part('quizHeader');
 session_start();
 $_SESSION['post_id'] = get_the_ID();
 ?>
 
-
-
 <?php if (get_field('activate_banner')) { ?>
-  <section id="banner" style="height:<?php echo get_field('banner_height'); ?>px; background:url('<?php echo get_field('banner_image'); ?>') no-repeat center center; background-size:cover;">
-  </section>
+    <section id="banner" style="height:<?php echo get_field('banner_height'); ?>px; background:url('<?php echo get_field('banner_image'); ?>') no-repeat center center; background-size:cover;">
+    </section>
 <?php } ?>
 
 <?php
 while (have_posts()) {
-  the_post(); ?>
+    the_post();
+    ?>
 
-  <?php $url = 'https://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-  $completedThisQuiz = false;
+    <?php
+    $url = 'https://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    $completedThisQuiz = false;
 
-  if (strpos($url, 'completedQuiz') !== false) {
-    $completedThisQuiz = true;
-  } else { ?>
-    <div class="quizBg"></div>
-    <div class="popup popupQuizVideo <?php $video = get_field('quiz_video');
-                                      if ($video) {
-                                        echo 'video_modal';
-                                      } ?>">
-      <div class="modal_content">
-        <?php echo $video; ?>
-      </div>
-    </div>
-  <?php
-    wp_enqueue_script("video-JS", get_template_directory_uri() . '/js/videoModal.js');
-  }
-  ?>
-  <!--Lines 17-28 Checks if Quiz has already been completed , if quiz has not been completed adds Video modal else dosent display video modal-->
-
-
-
-
-  <div class="page-banner quizContainer" ses="<?php echo $_SESSION['user_id'] ?>" id="currentpostid" dataType=<?php echo get_the_ID() ?>>
-    <div class="">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <a class="metabox__blog-home-link backHomeLink" href="<?php echo get_bloginfo('url'); ?>">
-          < NaturVet University Catalog</a>
-      </div>
-    </div>
-    <div class="quizLine"></div>
-    <div class=" quizTitle">
-      <h1 class=""><?php the_title(); ?></h1>
-      <div class="page-banner__intro quizText">
-        <!--Line 45 adds the "Quiz text" field associated to quiz in the "Quizzes" Tab in the back end-->
-        <?php the_field('quiz_text'); ?>
-
-        <div class="collapse" id="collapseVideo">
-          <?php the_field('quiz_video'); ?>
+    if (strpos($url, 'completedQuiz') !== false) {
+        $completedThisQuiz = true;
+    } else {
+    ?>
+        <div class="quizBg"></div>
+        <div class="popup popupQuizVideo <?php echo get_field('quiz_video') ? 'video_modal' : ''; ?>">
+            <div class="modal_content">
+                <?php echo get_field('quiz_video'); ?>
+            </div>
         </div>
+    <?php
+        wp_enqueue_script("video-JS", get_template_directory_uri() . '/js/videoModal.js');
+    }
+    ?>
 
-
-        <?php
-
-        if (!$completedThisQuiz) {
-          the_field('quiz');
-        } ?>
-        <!--Line 55-57 adds the "Quiz" field associated to quiz in the "Quizzes" Tab in the back end-->
-
-        <div class="completedQuizBottonInfo">
-          <h3><?php echo $completedText ?></h3>
-          <a href="<?php echo get_bloginfo('url'); ?>"><button class="top-btns">Return to Libary</button></a>
+    <div class="page-banner quizContainer" ses="<?php echo $_SESSION['user_id'] ?>" id="currentpostid" dataType=<?php echo get_the_ID() ?>>
+        <div class="">
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <a class="metabox__blog-home-link backHomeLink" href="<?php echo get_bloginfo('url'); ?>">
+                    < NaturVet University Catalog</a>
+            </div>
         </div>
-      </div>
+        <div class="quizLine"></div>
+        <div class="quizTitle">
+            <h1><?php the_title(); ?></h1>
+            <div class="page-banner__intro quizText">
+                <?php the_field('quiz_text'); ?>
+
+                <div class="collapse" id="collapseVideo">
+                    <?php the_field('quiz_video'); ?>
+                </div>
+
+                <?php
+                // Muestra el cuestionario si no se ha completado este quiz
+                if (!$completedThisQuiz) {
+                    the_field('quiz');
+                }
+                ?>
+
+                <div class="completedQuizBottonInfo">
+                    <h3><?php echo $completedText; ?></h3>
+                    <a href="<?php echo get_bloginfo('url'); ?>"><button class="top-btns">Return to Library</button></a>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 
-
-<?php }
+<?php
+}
 
 get_footer();
-
 ?>
