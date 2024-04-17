1b01f1abefffa66f16b25d1ed0ac24d40b0bd5df
Gabriel <gabo5612@gmail.com>
2024-04-17 12:31:17 -0400

    test

--- single-quiz.php
+++ single-quiz.php
@@ -230,7 +230,7 @@ while (have_posts()) {
                 ?>
 
                 <div class="completedQuizBottonInfo">
-                    <h3><?php echo $completedText; ?></h3>
+                    <h3><?php var_dump($completedText) ; ?></h3>
                     <a href="<?php echo get_bloginfo('url'); ?>"><button class="top-btns">Return to Library</button></a>
                 </div>
             </div>
