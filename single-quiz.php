2b230f849d6bc923396229128fce873d7dcb3350
Gabriel <gabo5612@gmail.com>
2024-04-17 12:29:10 -0400

    test

--- single-quiz.php
+++ single-quiz.php
@@ -163,7 +163,9 @@
   }
 </style>
 
-
+<?php
+require_once '/page-home.php';
+?>
 <?php
 if (!get_current_user_id()) {
     wp_redirect(home_url());
@@ -239,7 +241,4 @@ while (have_posts()) {
 }
 
 get_footer();
-?>
-<?php
-require_once '/page-home.php';
 ?>
