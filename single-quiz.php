ea534ba033517e981d61a8d685db620ffbc63d01
Gabriel <gabo5612@gmail.com>
2024-04-17 11:07:17 -0400

    fix

--- single-quiz.php
+++ single-quiz.php
@@ -150,83 +150,77 @@
     }
   }
 </style>
-
 <?php
 if (!get_current_user_id()) {
-  wp_redirect(home_url());
+    wp_redirect(home_url());
 }
-//Line 2-4 checks if thier is a active user, else redirect unknown user to home page
+//Line 2-4 checks if there is an active user, else redirect unknown user to home page
 get_template_part('quizHeader');
 session_start();
 $_SESSION['post_id'] = get_the_ID();
 ?>
 
 <?php if (get_field('activate_banner')) { ?>
-  <section id="banner" style="height:<?php echo get_field('banner_height'); ?>px; background:url('<?php echo get_field('banner_image'); ?>') no-repeat center center; background-size:cover;">
-  </section>
+    <section id="banner" style="height:<?php echo get_field('banner_height'); ?>px; background:url('<?php echo get_field('banner_image'); ?>') no-repeat center center; background-size:cover;">
+    </section>
 <?php } ?>
 
 <?php
 while (have_posts()) {
-  the_post(); ?>
+    the_post(); ?>
 
-  <?php $url = 'https://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-  $completedThisQuiz = false;
+    <?php
+    $completedThisQuiz = false;
+    $user_id = get_current_user_id();
+    $quiz_completed = get_user_meta($user_id, 'completed_quiz_id', true);
+    if ($quiz_completed == get_the_ID()) {
+        $completedThisQuiz = true;
+    }
+    ?>
 
-  if (strpos($url, 'completedQuiz') !== false) {
-    $completedThisQuiz = true;
-  } else { ?>
-    <div class="quizBg"></div>
-    <div class="popup popupQuizVideo <?php $video = get_field('quiz_video');
-                                      if ($video) {
-                                        echo 'video_modal';
-                                      } ?>">
-      <div class="modal_content">
-        <?php echo $video; ?>
-      </div>
+    <!--Lines 17-28 Checks if Quiz has already been completed, if quiz has not been completed adds Video modal else doesn't display video modal-->
+    <?php if (!$completedThisQuiz) { ?>
+        <div class="quizBg"></div>
+        <div class="popup popupQuizVideo <?php $video = get_field('quiz_video');
+                                            if ($video) {
+                                                echo 'video_modal';
+                                            } ?>">
+            <div class="modal_content">
+                <?php echo $video; ?>
+            </div>
+        </div>
+        <?php wp_enqueue_script("video-JS", get_template_directory_uri() . '/js/videoModal.js'); ?>
+    <?php } ?>
+
+
+    <div class="page-banner quizContainer" ses="<?php echo $_SESSION['user_id'] ?>" id="currentpostid" dataType=<?php echo get_the_ID() ?>>
+        <div class="container container--narrow page-section">
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <a class="metabox__blog-home-link backHomeLink" href="<?php echo get_bloginfo('url'); ?>">
+                    < NaturVet University Catalog</a>
+            </div>
+        </div>
+        <div class="quizLine"></div>
+        <div class="quizTitle">
+            <h1 class=""><?php the_title(); ?></h1>
+            <div class="page-banner__intro quizText">
+                <!--Line 45 adds the "Quiz text" field associated to quiz in the "Quizzes" Tab in the back end-->
+                <?php the_field('quiz_text'); ?>
+
+                <div class="collapse" id="collapseVideo">
+                    <?php the_field('quiz_video'); ?>
+                </div>
+
+                <?php if (!$completedThisQuiz) {
+                    the_field('quiz');
+                } ?>
+                <!--Line 55-57 adds the "Quiz" field associated to quiz in the "Quizzes" Tab in the back end-->
+            </div>
+        </div>
     </div>
-  <?php
-    wp_enqueue_script("video-JS", get_template_directory_uri() . '/js/videoModal.js');
-  }
-  ?>
-  <!--Lines 17-28 Checks if Quiz has already been completed , if quiz has not been completed adds Video modal else dosent display video modal-->
-
-
-
-
-  <div class="page-banner quizContainer" ses="<?php echo $_SESSION['user_id'] ?>" id="currentpostid" dataType=<?php echo get_the_ID() ?>>
-    <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <a class="metabox__blog-home-link backHomeLink" href="<?php echo get_bloginfo('url'); ?>">
-          < NaturVet University Catalog</a>
-      </div>
-    </div>
-    <div class="quizLine"></div>
-    <div class=" quizTitle">
-      <h1 class=""><?php the_title(); ?></h1>
-      <div class="page-banner__intro quizText">
-        <!--Line 45 adds the "Quiz text" field associated to quiz in the "Quizzes" Tab in the back end-->
-        <?php the_field('quiz_text'); ?>
-
-<div class="collapse" id="collapseVideo">
-  <?php the_field('quiz_video'); ?>
-</div>
-
-
-        <?php
-
-        if (!$completedThisQuiz) {
-          the_field('quiz');
-        } ?>
-        <!--Line 55-57 adds the "Quiz" field associated to quiz in the "Quizzes" Tab in the back end-->
-      </div>
-    </div>
-  </div>
-
 
 <?php }
 
 get_footer();
 
 ?>
-
