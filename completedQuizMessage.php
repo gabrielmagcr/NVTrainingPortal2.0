67d692a3a747f831d0b639063cbf9fca77ed7884
Gabriel <gabo5612@gmail.com>
2024-04-18 13:00:13 -0400

    completedMessage

---
+++ completedQuizMessage.php
@@ -0,0 +1,4 @@
+<div class="completedQuizBottonInfo">
+                    <h3>Great job, you've <br> completed this quiz</h3>
+                    <a href="<?php echo get_bloginfo('url'); ?>"><button class="top-btns">Return to Library</button></a>
+</div>
