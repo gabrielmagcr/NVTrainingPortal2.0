36bbf315aec51b4d8bd52fdef93beb48c52f2f27
Gabriel <gabo5612@gmail.com>
2024-04-22 11:10:33 -0400

    text styled

--- page-completedallquizzes.php
+++ page-completedallquizzes.php
@@ -1,116 +1,132 @@
 <style>
-.completedContentLeft {
-    text-align: center;
-}
-.completedForm{
-    width: 500px;
-    margin: 0 auto;
-    margin-top: 2%;
-    height: 76%;
-}
+    .completedContentLeft {
+        text-align: center;
+    }
+
+    .completedContentLeft h2 {
+        font-family: "Brandon Grotesque", sans-serif;
+        font-size: 72px;
+        font-weight: 600;
+        text-transform: uppercase;
+    }
+    .completedContentLeft p{
+        font-family: Montserrat;
+        font-size: 18px;
+    }
+
+    .completedForm {
+        width: 500px;
+        margin: 0 auto;
+        margin-top: 2%;
+        height: 76%;
+    }
+
+    @media (min-width:1400px) {
+        .completedContentLeft h2 {
+            font-size: 72px;
+        }
+    }
 </style>
-<?php 
- get_header();
- $userCompletedAllQuizzes=false;
- //Line 5-13 Checks database to see the number of quizzes  the current user completed
- $quizAmountCompleted = new WP_Query(array(
-        'post_type' => 'completed-quizzes',
-        'meta_query' => array(
-            array(
-                'key' => 'user_id',
-                'compare' => '=',
-                'value' => get_current_user_id()
-            ))
-        ));
-$userQuizCompleted=count($quizAmountCompleted->posts);
+<?php
+get_header();
+$userCompletedAllQuizzes = false;
+//Line 5-13 Checks database to see the number of quizzes  the current user completed
+$quizAmountCompleted = new WP_Query(array(
+    'post_type' => 'completed-quizzes',
+    'meta_query' => array(
+        array(
+            'key' => 'user_id',
+            'compare' => '=',
+            'value' => get_current_user_id()
+        )
+    )
+));
+$userQuizCompleted = count($quizAmountCompleted->posts);
 wp_reset_query();
 
 //Line 18-21 Check database to see the number of quizzes available
 $QuizzesAmount = new WP_Query(array(
-        'posts_per_page' => 10,
-        'post_type' => 'Quiz'
-      ));
+    'posts_per_page' => 10,
+    'post_type' => 'Quiz'
+));
 
-$totalQuizzesAvailable=count($QuizzesAmount->posts);
-    wp_reset_query();
- 
- if($userQuizCompleted>=$totalQuizzesAvailable){
-        $userCompletedAllQuizzes=true;
-}   
+$totalQuizzesAvailable = count($QuizzesAmount->posts);
+wp_reset_query();
 
-$completedForm=get_user_meta(get_current_user_id(),'completedGiftForm')[0];
-if(!$completedForm && $userCompletedAllQuizzes){ ?>
+if ($userQuizCompleted >= $totalQuizzesAvailable) {
+    $userCompletedAllQuizzes = true;
+}
+
+$completedForm = get_user_meta(get_current_user_id(), 'completedGiftForm')[0];
+if (!$completedForm && $userCompletedAllQuizzes) { ?>
 
 
-<div class="Completed pad-top">
-   <div class="completedContent">
-    <div class="completedContentLeft">
-  <h2 class="welcomeTitle">Please complete the below form.</h2>
-   <p>Your $5 Starbucks digital gift card will be emailed to you. <br> (Please allow 1-2 business days pending employee verification.)</p>
+    <div class="Completed pad-top">
+        <div class="completedContent">
+            <div class="completedContentLeft">
+                <h2>Please complete the below form.</h2>
+                <p>Your $5 Starbucks digital gift card will be emailed to you. <br> (Please allow 1-2 business days pending employee verification.)</p>
 
-   </div>  
-   <div class="completedForm"></div>
-</div>
-</div>  
+            </div>
+            <div class="completedForm"></div>
+        </div>
+    </div>
 
-<!--[if lte IE 8]>
+    <!--[if lte IE 8]>
 <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
 <![endif]-->
-<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
-<script>
+    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+    <script>
+        hbspt.forms.create({
+            portalId: "4878332",
+            formId: "0d96058d-1214-49ee-8ffb-be9e0093ce34",
+            target: '.completedForm',
+            onFormSubmit: function($form) {
 
- 
-  hbspt.forms.create({
-	portalId: "4878332",
-	formId: "0d96058d-1214-49ee-8ffb-be9e0093ce34",
-	target:'.completedForm',
-	onFormSubmit: function($form) {
-		
-		
-		jQuery.ajax ({
-            url:"<?php echo get_bloginfo('url'); ?>/wp-json/completed/v1/form" ,
-            type: 'POST',
-            dataType: 'JSON',
-            data: {           
-                'userGiftId':<?php echo get_current_user_id();?>
-                },
-            success: function (resp) {
-			
-				
-                if (resp.success) {
-                  
-                    
 
-                    }
-                else {
-                   
-                    alert ('Error: ' + resp.data) ;
-                    }
-                },
-            error: function (xhr, ajaxOptions, thrownError) {
-				
-                alert ('Request failed: ' + thrownError.message) ;
-                },
-            }) ;
-		}
-	   });
-    
+                jQuery.ajax({
+                    url: "<?php echo get_bloginfo('url'); ?>/wp-json/completed/v1/form",
+                    type: 'POST',
+                    dataType: 'JSON',
+                    data: {
+                        'userGiftId': <?php echo get_current_user_id(); ?>
+                    },
+                    success: function(resp) {
 
-</script>
-<?php }else{?>
-<!--If User has no completed the amount of quizzes required, User is redirected to home page -->
-      <div class="invalidAccess">
+
+                        if (resp.success) {
+
+
+
+                        } else {
+
+                            alert('Error: ' + resp.data);
+                        }
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+
+                        alert('Request failed: ' + thrownError.message);
+                    },
+                });
+            }
+        });
+    </script>
+<?php } else { ?>
+    <!--If User has no completed the amount of quizzes required, User is redirected to home page -->
+    <div class="invalidAccess">
         <div class="invalidAccessContent">
-        <h1>Sorry you don't have access to this page!</h1>
+            <h1>Sorry you don't have access to this page!</h1>
         </div>
-      </div>
-      <script>
-      jQuery("body").css("overflow","hidden");
-       setTimeout(function(){window.location.replace("<?php echo home_url()?>");},2500);
-      </script>
+    </div>
+    <script>
+        jQuery("body").css("overflow", "hidden");
+        setTimeout(function() {
+            window.location.replace("<?php echo home_url() ?>");
+        }, 2500);
+    </script>
 <?php
 
-}?>
+} ?>
 <div class="whiteSpace"></div>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
