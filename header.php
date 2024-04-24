6e971ca4ca5ee4dbcd01a6ee2aaaee7a8c8ce133
Gabriel <gabo5612@gmail.com>
2024-04-24 11:45:21 -0400

    test

--- header.php
+++ header.php
@@ -190,9 +190,7 @@
         ?>
           <h2 class="h2welcome">Welcome <span><?php echo $user_info->user_login; ?></span></h2>
 
-          <?php if($completed_quizzes_count >= 1) :
-        
-          ?>
+          <?php if($completed_quizzes_count >= 1):?>
             <h2>You've completed <span><?php echo $completed_quizzes_count; ?>/<?php echo $total_quizzes_count; ?></span><br> training videos</h2>
         <?php endif;
         } ?>
