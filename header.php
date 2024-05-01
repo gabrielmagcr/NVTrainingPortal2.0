eabce7f7b6b83c8f30bc548e0100717899b83d8d
Gabriel <gabo5612@gmail.com>
2024-05-01 18:15:53 -0400

    title

--- header.php
+++ header.php
@@ -31,6 +31,7 @@
   <meta http-equiv="Expires" content="-1">
   <meta http-equiv="CACHE-CONTROL" content="NO-CACHE">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+  <title>Naturevet Training Portal</title>
 
   <style type="text/css">
    
