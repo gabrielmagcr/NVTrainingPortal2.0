549e12d660a6a9606eb9794e113955970387cfa0
Gabriel <gabo5612@gmail.com>
2024-07-08 12:11:27 -0400

    product line fix

--- productLine.php
+++ productLine.php
@@ -17,28 +17,28 @@ if( have_rows('product_line') ):
     // Loop through rows.
     while( have_rows('product_line') ) : the_row();
 
-    $productVideo1 = get_field('productVideo');
-    $pPlaceHolderImg = get_field('pPlaceHolderImg');
-    $pVideoTitle = get_field('pVideoTitle');
-    $pVideoText = get_field('pVideoText');
+    $product_video = get_field('product_video');
+    $product_place_holder_image = get_field('product_place_holder_image');
+    $product_video_title = get_field('product_video_title');
+    $product_video_text = get_field('product_video_text');
 ?>
 
         <div class="mainVideo productLineVideo">
             <div class="mainVideoImg productLineImg">
-                <img class="placeholder-img placeholder-imgPL lazy-load " data-src="<?php echo $pPlaceHolderImg1 ?>"
+                <img class="placeholder-img placeholder-imgPL lazy-load " data-src="<?php echo $product_place_holder_image ?>"
                     alt="Placeholder Image">
                 <img class="play-icon playIconProductVideo lazy-load" data-src="/wp-content/uploads/Button-play.svg"
                     alt="Play Button">
                 <video class="video" controls style="display: none;">
-                    <source src="<?php echo $productVideo1 ?>" type="video/mp4">
+                    <source src="<?php echo $product_video ?>" type="video/mp4">
                 </video>
             </div>
             <div class="mainVideoText productLineText lazy-load">
                 <h3>
-                    <?php echo $pVideoTitle1 ?>
+                    <?php echo $product_video_title ?>
                 </h3>
                 <p>
-                    <?php echo  $pVideoText1 ?>
+                    <?php echo  $product_video_text ?>
                 </p>
             </div>
         </div>
