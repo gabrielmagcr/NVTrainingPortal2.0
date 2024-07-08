a88b320658b8e0f4d3fa9a5a1db946aa5eb03fc7
Gabriel <gabo5612@gmail.com>
2024-07-08 13:26:11 -0400

    btn

--- productLine.php
+++ productLine.php
@@ -58,7 +58,7 @@ $productionSectionTitle = get_field('productionSectionTitle');
                         <?php if ($product_place_holder_image): ?>
                             <img class="placeholder-img placeholder-imgPL lazy-load" src="/wp-content/uploads/lazyload.gif" data-src="<?php echo esc_url($product_place_holder_image); ?>" data-placeholder="/wp-content/uploads/lazyload.gif" alt="Placeholder Image">
                         <?php endif; ?>
-                        <img class="play-icon playIconProductVideo lazy-load" src="/wp-content/uploads/lazyload.gif" data-src="/wp-content/uploads/Button-play.svg" data-placeholder="/wp-content/uploads/lazyload.gif" alt="Play Button">
+                        <img class="play-icon playIconProductVideo lazy-load"  data-src="/wp-content/uploads/Button-play.svg" data-placeholder="/wp-content/uploads/lazyload.gif" alt="Play Button">
                         <?php if ($product_video): ?>
                             <video class="video" controls style="display: none;">
                                 <source src="<?php echo esc_url($product_video); ?>" type="video/mp4">
