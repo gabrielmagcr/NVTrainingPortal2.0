047f7f711d204fa0a9a96e0b402c1f207721daaa
Gabriel <gabo5612@gmail.com>
2025-02-26 16:16:14 -0400

    fix main video

--- mainVideos.php
+++ mainVideos.php
@@ -1,33 +1,29 @@
 <div class="mainVideosContainer">
-	<?php 
-			if(have_rows('welcome_videos'));
-
-				while(have_rows('welcome_videos')): the_row();
-				$welcomeVideo = get_field('welcome_video')? get_field('welcome_video'): "";
-				$placeHolderImage = get_field('place_holder_image')? get_field('place_holder_image'): "";
-				$videoTitle = get_field('video_title')? get_field('video_title'): "";
-				$videoText = get_field('video_text')? get_field('video_text'): "";
-
-	
-	?>
-	<div class="mainVideo">
-		<div class="mainVideoImg">
-			<img class="placeholder-img" src="<?php echo $placeHolderImage ?>" alt="Placeholder Image">
-			<img class="play-icon playIconMainVideo" src="/wp-content/uploads/Button-play.svg" alt="Play Button">
-			<video class="video" controls style="display: none;">
-				<source src="<?php echo $welcomeVideo ?>" type="video/mp4">
-			</video>
-		</div>
-		<div class="mainVideoText">
-			<h3><?php echo $videoTitle ?></h3>
-			<p><?php echo $videoText  ?> </p>
-		</div>
-	</div>
-    <?php endwhile; ?>
-	<?php endif; ?>
-
+    <?php 
+    if(have_rows('welcome_videos')): 
+        while(have_rows('welcome_videos')): the_row();
+            $welcomeVideo = get_sub_field('welcome_video') ? get_sub_field('welcome_video') : "";
+            $placeHolderImage = get_sub_field('place_holder_image') ? get_sub_field('place_holder_image') : "";
+            $videoTitle = get_sub_field('video_title') ? get_sub_field('video_title') : "";
+            $videoText = get_sub_field('video_text') ? get_sub_field('video_text') : "";
+    ?>
+        <div class="mainVideo">
+            <div class="mainVideoImg">
+                <img class="placeholder-img" src="<?php echo esc_url($placeHolderImage); ?>" alt="Placeholder Image">
+                <img class="play-icon playIconMainVideo" src="/wp-content/uploads/Button-play.svg" alt="Play Button">
+                <video class="video" controls style="display: none;">
+                    <source src="<?php echo esc_url($welcomeVideo); ?>" type="video/mp4">
+                </video>
+            </div>
+            <div class="mainVideoText">
+                <h3><?php echo esc_html($videoTitle); ?></h3>
+                <p><?php echo esc_html($videoText); ?></p>
+            </div>
+        </div>
+    <?php 
+        endwhile;
+    endif;
+    ?>
 </div>
-
-
 
 <?php get_template_part('lineBreak'); ?>
