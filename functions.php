501815ed6819d1129a8a1e7e158ba4de0823e8ed
Gabriel <gabo5612@gmail.com>
2025-02-26 18:57:50 -0400

    svg option

--- functions.php
+++ functions.php
@@ -143,3 +143,9 @@ function my_theme_enqueue_scripts() {
   wp_enqueue_script('lazy-load', get_template_directory_uri() . '/js/scripts.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
+
+function permitir_svg($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'permitir_svg');
