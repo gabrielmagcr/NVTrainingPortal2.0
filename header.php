06e4f955b17310c252e0e0858190c565bf087b50
Gabriel <gabo5612@gmail.com>
2025-02-26 18:58:29 -0400

    header

--- header.php
+++ header.php
@@ -184,7 +184,7 @@
   <div class="bannerClass">
     <section id="banner"></section>
     <div class="shield lazy-load">
-      <img  src="./images/NV-shield.svg" class="lazy-load" alt="University Naturevet">
+      <img  src="/wp-content/uploads/Layer_1.svg" class="lazy-load" alt="University Naturevet">
       <div class="bannerInfo">
         <?php if (!is_user_logged_in()) { ?>
           <div class="logoutUser">
