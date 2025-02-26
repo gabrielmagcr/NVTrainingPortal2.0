a6abc6c4b165289e6afdb22dbc0b53ebf4bbc79d
Gabriel <gabo5612@gmail.com>
2025-02-26 16:48:33 -0400

    Product line grid

--- page-home.php
+++ page-home.php
@@ -83,95 +83,11 @@ $modalCompletedAllQuizzes = ($completedQuizzesCount >= $totalQuizzesCount);
 							<p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 						</div>
 					</div>
-
-					<?php
-					// Verifica si todos los cuestionarios están completos
-					if ($modalCompletedAllQuizzes) {
-						// Obtén el formulario completado (si existe) para el usuario actual
-						$modalCompletedForm = get_user_meta(get_current_user_id(), 'completedGiftForm', true);
-
-						// Si todos los cuestionarios están completos y el formulario aún no se ha completado, muestra el botón para recibir la tarjeta de regalo gratuita
-						if (!$modalCompletedForm) {
-					?>
-							<div class="CompletedLinkButton">
-								<a href="<?php echo get_bloginfo('url'); ?>/completedallquizzes/">
-									<button class="top-btns breathBtn">Click here to receive your <strong>FREE</strong> Starbucks gift card!</button>
-								</a>
-							</div>
-					<?php
-						}
-					}
-					?>
-
-					<?php
-					$modalQuizzesCompleted = new WP_Query(array(
-						'post_type' => 'completed-quizzes',
-						'meta_query' => array(
-							array(
-								'key' => 'user_id',
-								'compare' => '=',
-								'value' => get_current_user_id()
-							)
-						)
-					));
-					if ($modalQuizzesCompleted->found_posts >= $modalTotalQuizzesAvailable->found_posts) {
-						$modalCompletedAllQuizzes = true;
-					} else {
-						$modalCompletedAllQuizzes = false;
-					}
-					?>
+					<?php get_template_part('productLineGrid'); ?>
+					
 			</div>
 		</div>
-		<div class="quizContainer">
-			<?php
-					$homepageQuizzes = new WP_Query(array(
-						'posts_per_page' => 10,
-						'post_type' => 'Quiz'
-					));
-					// Loops Through the list of Quizzes available and adds them to the DOM
-					while ($homepageQuizzes->have_posts()) {
-						$homepageQuizzes->the_post(); ?>
-
-				<?php
-						$quizCompleted = new WP_Query(array(
-							'post_type' => 'completed-quizzes',
-							'meta_query' => array(
-								array(
-									'key' => 'completed_quiz_id',
-									'compare' => '=',
-									'value' => get_the_ID()
-								),
-								array(
-									'key' => 'user_id',
-									'compare' => '=',
-									'value' => get_current_user_id()
-								)
-							)
-						));
-
-						// Check if the quiz is completed or not
-						if ($quizCompleted->found_posts) {
-				?>
-					<div class="quiz completedQuiz hoverAnimation">
-						<a href="<?php the_permalink() ?><?php echo "?completedQuiz"; ?>" class="quiz-link">
-							<img class='quizzImage quizzImageCompleted-' src="<?php the_field('quiz_bg_image') ?>">
-							<img class='play-icon' src="/wp-content/uploads/Button-play.svg">
-						</a>
-						<span><img class="completedCheck" src="/wp-content/uploads/Vector.svg"><?php echo get_quiz_title(get_the_ID()); ?></span>
-					</div>
-				<?php
-						} else { ?>
-					<div class="quiz hoverAnimation">
-						<a href="<?php the_permalink() ?>" class="quiz-link">
-							<img class='quizzImage' src="<?php the_field('quiz_bg_image') ?>">
-							<img class='play-icon' src="/wp-content/uploads/Button-play.svg">
-						</a>
-						<span><?php echo get_quiz_title(get_the_ID()); ?></span>
-					</div>
-			<?php }
-					}
-			?>
-		</div>
+		
 
 	</div>
 
