d236690232ae61c6bf19e365d2580a2a52ed3217
Gabriel <gabo5612@gmail.com>
2025-02-27 11:30:25 -0400

    button test

--- page-home.php
+++ page-home.php
@@ -79,7 +79,7 @@ $modalCompletedAllQuizzes = ($completedQuizzesCount >= $totalQuizzesCount);
 
 				
 					<?php get_template_part('productLineGrid'); ?>
-					
+					<button class="hs-cta-trigger-button hs-cta-trigger-button-186683445849">test</button>
 			</div>
 		</div>
 		
