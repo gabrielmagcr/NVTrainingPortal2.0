68c4c0e1246d2d6c09b2ffb6be18a3c722dff97d
Gabriel <gabo5612@gmail.com>
2025-02-27 18:10:46 -0400

    style

--- survey.php
+++ survey.php
@@ -4,8 +4,8 @@
  */
 get_header();
 ?>
-
+<div class="survey">
 <script src="https://js.hsforms.net/forms/embed/4878332.js" defer></script>
 <div class="hs-form-frame" data-region="na1" data-form-id="ffde122c-fa06-437a-b985-d58a852d08a8" data-portal-id="4878332"></div>
-
+</div>
 <?php get_footer(); ?>
