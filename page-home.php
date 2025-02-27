fae656981e0f26058b5a5890fe02bfa8d5835f1a
Gabriel <gabo5612@gmail.com>
2025-02-27 18:14:26 -0400

    fix

--- page-home.php
+++ page-home.php
@@ -79,8 +79,7 @@ $modalCompletedAllQuizzes = ($completedQuizzesCount >= $totalQuizzesCount);
 
 				
 					<?php get_template_part('productLineGrid'); ?>
-					<button class="hs-cta-trigger-button hs-cta-trigger-button-186683445849 cta-btn">Training Portal Quiz
-					</button>
+					<a href="/survey" class="hs-cta-trigger-button hs-cta-trigger-button-186683445849 cta-btn">Training Portal Quiz</a>
 			</div>
 		</div>
 		
