30fa6f8c001cdca07c6a3082737fa19cc16df550
Gabriel <gabo5612@gmail.com>
2025-02-27 18:07:59 -0400

    survey

---
+++ survey.php
@@ -0,0 +1,11 @@
+<?php
+/* 
+ Template Name: Survey
+ */
+get_header();
+?>
+
+<script src="https://js.hsforms.net/forms/embed/4878332.js" defer></script>
+<div class="hs-form-frame" data-region="na1" data-form-id="ffde122c-fa06-437a-b985-d58a852d08a8" data-portal-id="4878332"></div>
+
+<?php get_footer(); ?>
