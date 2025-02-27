2b16faf4dddc54ddd62a76432c1ca1e55d977ffa
Gabriel <gabo5612@gmail.com>
2025-02-27 12:55:01 -0400

    Giftcard

---
+++ template-giftcard.php
@@ -0,0 +1,117 @@
+<style>
+    .completedContent {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .completedContentLeft {
+        text-align: center;
+        width: 90%;
+    }
+
+    .completedContentLeft h2 {
+        font-family: "Brandon Grotesque", sans-serif;
+        color: #4D7F70;
+        font-size: 36px;
+        font-weight: 600;
+        text-transform: uppercase;
+    }
+
+    .completedContentLeft p {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 18px;
+    }
+
+    .completedForm {
+        padding-left: 25px;
+        margin-bottom:40px
+    }
+
+    .hs-button {
+        border-radius: 30px !important;
+    }
+
+    @media (min-width:767px) {
+        .completedForm {
+            width: 530px;
+            padding-left: 25px;
+        }
+
+        .completedContentLeft h2 {
+            font-size: 48px;
+        }
+    }
+
+    @media (min-width:1400px) {
+        .completedContentLeft h2 {
+            font-size: 72px;
+        }
+
+        .completedContentLeft {
+            width: 50%;
+        }
+    }
+</style>
+<?php
+/* 
+ Template Name: Hubspot-Giftcard
+ */
+get_header();
+?>
+
+    <div class="Completed pad-top">
+        <div class="completedContent">
+            <div class="completedContentLeft">
+                <h2>Thank you/h2>
+                <p>Your $5 Starbucks digital gift card will be emailed to you. <br> (Please allow 1-2 business days pending employee verification.)</p>
+
+            </div>
+            <div class="completedForm"></div>
+        </div>
+    </div>
+
+    <!--[if lte IE 8]>
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+<![endif]-->
+    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+    <script>
+        hbspt.forms.create({
+            portalId: "4878332",
+            formId: "0d96058d-1214-49ee-8ffb-be9e0093ce34",
+            target: '.completedForm',
+            onFormSubmit: function($form) {
+
+
+                jQuery.ajax({
+                    url: "<?php echo get_bloginfo('url'); ?>/wp-json/completed/v1/form",
+                    type: 'POST',
+                    dataType: 'JSON',
+                    data: {
+                        'userGiftId': <?php echo get_current_user_id(); ?>
+                    },
+                    success: function(resp) {
+
+
+                        if (resp.success) {
+
+
+
+                        } else {
+
+                            alert('Error: ' + resp.data);
+                        }
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+
+                        alert('Request failed: ' + thrownError.message);
+                    },
+                });
+            }
+        });
+    </script>
+
+
+
+
+<?php get_footer(); ?>
