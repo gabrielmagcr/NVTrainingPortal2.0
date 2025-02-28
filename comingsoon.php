74679fddc8b1bcf5e11af90ad4269d28b05aeb93
Gabriel <gabo5612@gmail.com>
2025-02-28 19:08:11 -0400

    coming soon

---
+++ comingsoon.php
@@ -0,0 +1,45 @@
+<?php 
+$comingsoon = get_field('comingsoon') ? get_field('comingsoon') : "";
+?>
+
+
+<div class="productLineContainer training-container">
+    <h2 class="welcomeTitle test">
+        <?php echo esc_html($comingsoon); ?>
+    </h2>
+    <div class="quizContainer">
+        <?php
+        // Check rows exists.
+        if( have_rows('product_line') ):
+            // Loop through rows.
+            while( have_rows('product_line_grid') ) : the_row();
+
+                $product_video = get_sub_field('product_video');
+                $product_place_holder_image = get_sub_field('product_place_holder_image');
+                $product_video_title = get_sub_field('product_video_title');
+                ?>
+                <div class="quiz">
+                <?php if ($product_place_holder_image): ?>
+                    <img class="quizzImage <?php echo $product_video ? 'placeholder-img' : ''; ?>" src="<?php echo esc_url($product_place_holder_image); ?>" alt="Placeholder Image">
+
+                        <?php endif; ?>
+                        <?php if ($product_video): ?>
+                            <video class="video" controls controlsList="nodownload" style="display: none;">
+                                <source src="<?php echo esc_url($product_video); ?>" type="video/mp4">
+                            </video>
+                        <?php endif; ?>
+                        <?php if ($product_video_title): ?>
+                            <span><?php echo esc_html($product_video_title); ?></span>
+                        <?php endif; ?>
+                   
+                </div>
+                <?php 
+            // End loop.
+            endwhile;
+        endif;
+        ?>
+    </div>
+</div>
+<?php get_template_part('lineBreak'); ?>
+
+
