fc58c80c41710276ae335cc296aee606023f980c
Gabriel <gabo5612@gmail.com>
2025-02-28 16:47:05 -0400

    password

---
+++ template-lost-password.php
@@ -0,0 +1,71 @@
+<div class="login">
+<a class="login-logo" href="https://training.naturvet.com/"><img src="https://training.naturvet.com/wp-content/uploads/2018/08/naturvet-logo_green.png"/></a>
+<?php
+/* 
+ Template Name: new-lost-password
+ */
+the_content(); ?>
+
+
+<style>
+    body{
+        margin:0;
+        color: #3c434a;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    }
+    .login{
+  display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+      background: #f0f0f1;
+}
+.fieldset-username, .fieldset-password, .fieldset-remember, .fieldset-user_password, .fieldset-robo, .fieldset-user_email{
+  border: none;
+  margin: 0;
+  padding: 0;
+}
+.field{
+    padding-top: 3px;
+}
+.login-logo img{
+    height: 65px;
+    width: 320px;
+    padding-bottom: 50px;
+}
+.login form{
+    background: white;
+    padding: 30px;
+    border: solid 1px #b4b4b5;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    border-radius: 6px;
+}
+.input-text, .input-email{
+    width: 100%;
+    border: solid 1px #b4b4b5;
+    border-radius: 3px;
+    padding: 5;
+}
+.button{
+    background: #135e96;
+    padding: 10px;
+    border: navajowhite;
+    color: #fff;
+    width: auto;
+    border-radius: 6px;
+    margin: 0 auto;
+}
+.wpum-action-links{
+    list-style: none;
+    font-size: 12px;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+}
+.wpum-action-links a{
+    text-decoration:none;
+}
+    </style>
